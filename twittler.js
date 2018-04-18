9bb3a210a8df6b6161bc5ab5cfbd13f642b50a65
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-04-17 17:31:12 -0700

    Re-did twittler exercise for additional practice

--- twittler.js
+++ twittler.js
@@ -1,49 +1,48 @@
 $(document).ready(function(){
-  displayTweets();
+  function updateTweets(){
+	$('body').find('div').remove();
+	var index = streams.home.length - 1;
+	while(index >= 0){
+	  var tweet = streams.home[index];
+	  var $tweet = $('<div class=tweet-content></div>');
+	  $tweet.text(tweet.message);
+	  $tweet.appendTo('body');
+	  var $userName = $('<div class=name></div>');
+	  $userName.text('@'+tweet.user)
+	  $userName.prependTo($('.tweet-content').last());
+	  $('.tweet-content').last().addClass(tweet.user)
+	  var $time = $('<div class=time-created></div>');
+	  $time.text(tweet.created_at)
+	  $time.appendTo($('.tweet-content').last());
+	  index -= 1;
+	}
+  };
 
-  function displayTweets(names){
-    var $body = $('.feed');
-    $body.html('');
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div class="tweet"></div>');
-      var $tweetUser = $('<h2 class="tweetUser">@' + tweet.user + ':</div>');
-      $tweetUser.data('username',tweet.user);
-      $tweetUser.appendTo($tweet);
-      var $message = $('<p>' + tweet.message + '</p>');
-      $message.appendTo($tweet);
-      var $tweetTime = $('<h5>' + tweet.created_at + '</h5>');
-      $tweetTime.appendTo($tweet);
-      $tweet.appendTo($body);
-      index -=1;
-    }
-    $('#update-btn').click(displayTweets);
+  //Load tweets when first visiting page
+  updateTweets();
 
-    $('.tweetUser').click(function(event) {
-      window.scrollTo(0,0);
-      event.stopPropagation(); 
-      var $thisTweet = $(event.target);
-      var thisUser = $thisTweet.data('username');
-      var userFeed = streams.users[thisUser];
-      var index = userFeed.length - 1;
-      $body.html('');
-      while(index >= 0) {
-        var tweet = userFeed[index];
-        var $tweet = $('<div class="tweet"></div>');
-        var $tweetUser = $('<h2 class="tweetUser">@' + tweet.user + ':</div>');
-        $tweetUser.data('username', tweet.user);
-        $tweetUser.appendTo($tweet);
-        var $tweetMessage = $('<p>' + tweet.message + '</p>');
-        $tweetMessage.appendTo($tweet);
-        var $tweetTimestamp = $('<h5>' + tweet.created_at + '</h5>');
-        $tweetTimestamp.appendTo($tweet);
-        $tweet.appendTo($body);
-        index -= 1;
-      }
-    });
-  }
-  //$('#update-btn').on('click',displayTweets)
-}); 
+  //Update tweet feed when clicking update tweet button
+  $('.update-btn').on('click',updateTweets);
+  
+  //Highlight username when mouse enters div (and unhighlights when it leaves)
+  $('body').on('mouseenter','.name',function(){
+  	$(this).toggleClass('highlighted')
+  })
+
+  $('body').on('mouseleave','.name',function(){
+  	$(this).toggleClass('highlighted')
+  })
+
+
+  //Show only user's tweets when you click on username
+  $('body').on('click','.name',function(){
+  	var clickedName = $(this).parent().attr('class').split(' ')[1];
+    $('body').find('div').hide();
+    $('.' + clickedName).show();
+    $('.'+ clickedName).children().show();
+  });
+
+});
+
 
 
